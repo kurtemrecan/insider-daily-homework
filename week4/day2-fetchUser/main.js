17c2af38cd4e48a697e61acd716ca365af7576fa
Emre Can Kurt <can.kurt764@gmail.com>
2025-03-13 01:31:13 +0300

    added css and fixed storageTime

--- week4/day2-fetchUser/main.js
+++ week4/day2-fetchUser/main.js
@@ -25,10 +25,12 @@ function displayUsers(users) {
     const userCard = $('<div>')
       .addClass('user-card')
       .append(
-        $('<h2>').text(`Ad: ${user.name}`),
-        $('<p>').text(`Email: ${user.email}`),
-        $('<p>').text(
-          `Adres: ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}, ${user.address.geo.lat} / ${user.address.geo.lng} `
+        $('<h2>').text(`${user.name}`),
+        $('<p>').html(
+          `<span class="bold-text">Email:</span> <br> ${user.email}`
+        ),
+        $('<p>').html(
+          `<span class="bold-text">Adres:</span> <br> ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}, ${user.address.geo.lat} / ${user.address.geo.lng} `
         ),
         $('<button>')
           .addClass('delete-btn')
@@ -47,7 +49,7 @@ function displayUsers(users) {
 
 function saveToStorage(users) {
   localStorage.setItem('userData', JSON.stringify(users));
-  localStorage.setItem('storageTime', Date.now() + 10 * 1000); // şimdiki zamandan itibaren 24 saat(milisaniye cinsten oluşturduk date.nowdan ötürü)
+  localStorage.setItem('storageTime', Date.now() + 24 * 60 * 60 * 1000); // şimdiki zamandan itibaren 24 saat(milisaniye cinsten oluşturduk date.nowdan ötürü)
 }
 
 function checkLocalStorage() {
@@ -55,7 +57,7 @@ function checkLocalStorage() {
   const storageTime = localStorage.getItem('storageTime');
 
   if (storageTime && Date.now() < parseInt(storageTime)) {
-    console.log("localStorage'dan veri okunuyor");
+    //console.log("localStorage'dan veri okunuyor");
     const users = JSON.parse(userData);
     displayUsers(users);
   } else {
@@ -65,9 +67,71 @@ function checkLocalStorage() {
 
 $(document).ready(function () {
   checkLocalStorage();
-});
 
-/*
-kalanlar;
--Basit Bir Arayüz Oluşturma (variable tanımlayıp style tagiyle mi yoksa doğruda css() metodu kullanarak mı)
-*/
+  const styles = `
+    body {
+      background-color: #2d2d2d;
+      color: #e0e0e0;
+      font-family: Arial, sans-serif; 
+      margin: 0;
+      padding: 0;
+    }
+
+    h2 {
+      color: #fff;
+      text-shadow: 2px 2px 8px rgba(3, 178, 178, 0.3);
+    }
+
+    .ins-api-users {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 20px; 
+      justify-content: center; 
+      max-width: 1200px;
+      margin: 20px auto;
+      padding: 20px;
+    }
+
+    .user-card {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      background-color: #444444;
+      border-radius: 10px; 
+      box-shadow: 0 4px 10px rgba(255, 255, 255, 0.3); 
+      padding: 20px;
+      width: 300px;
+      transition: transform 0.4s ease;
+      line-height: 1.35;
+    }
+    
+    .user-card:hover {
+      transform: translateY(-4px);
+      box-shadow: 0 10px 20px rgba(255,255,255,0.5);
+    }
+    
+    .bold-text {
+    font-weight: bold;
+    }
+
+    .delete-btn {
+      background-color:rgb(59, 133, 144);
+      color: white;
+      border-radius: 25px;
+      padding: 8px 20px;
+      text-align: center;
+      font-size: 14px;
+      border: none;
+      align-self: flex-start;
+      transition: transform 0.3s ease;
+      cursor: pointer;
+    }
+
+    .delete-btn:hover {
+      background-color:rgb(211, 120, 120);
+      transform: translateY(-3px)
+    }
+  `;
+
+  $('<style>').text(styles).appendTo('head');
+});
